1a677914bda703e57ebbaf28d0ec63eb9c53305d
Mostafafar <farkhondei82@gmail.com>
2025-07-15 22:10:40 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1080,6 +1080,7 @@ async def select_items(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
         await query.edit_message_text(
             text=message,
             reply_markup=InlineKeyboardMarkup(keyboard)
+        )
         return States.CONFIRM_TOTALS
 
     elif query.data == "compensate":
