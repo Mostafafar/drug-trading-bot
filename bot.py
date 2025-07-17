9342af3572f2c85c90d3eeca0465ceae985b0a18
Mostafafar <farkhondei82@gmail.com>
2025-07-17 23:53:19 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ logger = logging.getLogger(__name__)
 DB_CONFIG = {
     'dbname': 'drug_trading',
     'user': 'postgres',
-    'password': 'yourpassword',
+    'password': 'f13821382',
     'host': 'localhost',
     'port': '5432'
 }
