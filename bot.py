1f72003fed65f4dde652e6caf9121469999695ae
Mostafafar <farkhondei82@gmail.com>
2025-07-22 17:17:21 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3763,7 +3763,7 @@ async def run_bot():
                 ]
             },
             fallbacks=[CommandHandler("cancel", cancel)],
-            per_message=True,
+            per_message=False,
             per_chat=True,
             per_user=True,
             name="main_conversation"
